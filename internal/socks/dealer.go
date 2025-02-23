84612b6d98dfae04ab8737b7551fee3963c16563
bob <irooniam@gmail.com>
2025-02-22 23:35:16 -0800

    initial code for dealer and router sockets

---
+++ internal/socks/dealer.go
@@ -0,0 +1,42 @@
+package socks
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/pebbe/zmq4"
+)
+
+type ZDealer struct {
+	ID   string
+	In   chan []byte
+	Out  chan []byte
+	Sock *zmq4.Socket
+}
+
+func (d *ZDealer) Connect(port int) error {
+	conn := fmt.Sprintf("0.0.0.0:%d", port)
+	err := d.Sock.Bind(conn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewDealer(ID string) (*ZDealer, error) {
+	dealer, err := zmq4.NewSocket(zmq4.DEALER)
+	if err != nil {
+		return &ZDealer{}, err
+	}
+
+	err = dealer.SetIdentity(ID)
+	if err != nil {
+		return &ZDealer{}, errors.New(fmt.Sprintf("Tried setting identity but got error: %s", err))
+	}
+
+	in := make(chan []byte)
+	out := make(chan []byte)
+
+	return &ZDealer{ID: ID, In: in, Out: out, Sock: dealer}, nil
+}
---
+++ internal/socks/router.go
@@ -0,0 +1,42 @@
+package socks
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/pebbe/zmq4"
+)
+
+type ZRouter struct {
+	ID   string
+	In   chan []byte
+	Out  chan []byte
+	Sock *zmq4.Socket
+}
+
+func (d *ZRouter) Bind(port int) error {
+	conn := fmt.Sprintf("0.0.0.0:%d", port)
+	err := d.Sock.Bind(conn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewRouter(ID string) (*ZRouter, error) {
+	dealer, err := zmq4.NewSocket(zmq4.DEALER)
+	if err != nil {
+		return &ZRouter{}, err
+	}
+
+	err = dealer.SetIdentity(ID)
+	if err != nil {
+		return &ZRouter{}, errors.New(fmt.Sprintf("Tried setting identity but got error: %s", err))
+	}
+
+	in := make(chan []byte)
+	out := make(chan []byte)
+
+	return &ZRouter{ID: ID, In: in, Out: out, Sock: dealer}, nil
+}
