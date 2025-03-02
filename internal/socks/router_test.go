d8595f7e9438bfbf836620e569f8e5018c21cf38
bob <irooniam@gmail.com>
2025-03-01 18:01:19 -0800

    updated basic test of router socket

--- internal/socks/router_test.go
+++ internal/socks/router_test.go
@@ -1,18 +1,27 @@
 package socks_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/Irooniam/msg/internal/socks"
 )
 
 func TestNewRouterHappyPath(t *testing.T) {
+	var wg sync.WaitGroup
 	r, err := socks.NewZRouter("router-test")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if err := r.Bind("127.0.0.1", 9876); err != nil {
+	if err := r.Bind("127.0.0.1:9888"); err != nil {
 		t.Error(err)
 	}
+
+	go r.Run()
+	wg.Add(1)
+
+	r.In <- [][]byte{[]byte("dealer"), []byte("payload")}
+	r.Done <- true
+	wg.Done()
 }
